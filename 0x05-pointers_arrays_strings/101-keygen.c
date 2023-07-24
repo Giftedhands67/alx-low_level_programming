548a1e6f9a2013cebe068a6a9c2522db387d9652
Giftedhands67 <clemzycul@gmail.com>
2023-07-24 16:50:31 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - program that generates random valid
  * passwords for the program 101-crackme
- *
  * Return: Always 0 (Success)
  */
 int main(void)
