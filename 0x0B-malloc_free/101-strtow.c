f1468635f2322702532b59b2cc8922feed6d1a62
Giftedhands67 <clemzycul@gmail.com>
2023-08-07 18:41:17 -0700

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -34,11 +34,10 @@ int count_word(char *s)
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
-char **strtow(char *str);
+char **strtow(char *str)
 {
-	{
-		char **matrix, *tmp;
-		int i, k = 0, len = 0, words, c = 0, start, end;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
 		while (*(str + len))
 			len++;
@@ -75,4 +74,4 @@ char **strtow(char *str);
 		matrix[k] = NULL;
 
 		return (matrix);
-	}
+}
